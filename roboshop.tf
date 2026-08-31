a8ad78142314f0375102f918d2224f22e0d1db9e
Gopi Chand <gopi110531@gmail.com>
2026-08-31 14:36:44 -0500

    Roboshop code

--- roboshop.tf
+++ roboshop.tf
@@ -8,100 +8,17 @@ variable "ami"{
     default = "ami-0220d79f3f480ecf5"
     }
 
-resource "aws_instance" "frontend" {
+variable "instances"{
+    default = ["frontend","mongodb","catalogue","redis","user","cart","mysql","shipping","rabbitmq","payment","dispatch"]
+    }
+resource "aws_instance" "instance" {
+    count = length(var.instances)
   ami           = var.ami
   instance_type = var.instance_type
 
   tags = {
-    Name = "frontend"
+    Name = var.instances[count.index]
   }
 }
 
-resource "aws_instance" "mongodb" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_instance" "redis" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_instance" "user" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_instance" "cart" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_instance" "mysql" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_instance" "shipping" {
-  ami           = var.ami
-  instance_type = var.instance_type
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_instance" "payment" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_instance" "dispatch" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "dispatch"
-  }
-}
 
