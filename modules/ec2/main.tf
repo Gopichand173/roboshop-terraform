09b4bb9471d40233b43a739349104dbd376dc88f
Gopi Chand <gopi110531@gmail.com>
2026-09-02 18:08:59 -0500

    Roboshop code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,14 +1,13 @@
 resource "aws_instance" "instance" {
   ami           = var.ami
   instance_type = var.instance_type
-  vpc_security_groups_ids      = [data.aws_security_group.allow-all.id]
+  vpc_security_group_ids      = [data.aws_security_group.allow-all.id]
 
   tags = {
     Name = "${var.name}-${var.env}"
   }
 
 }
-
 resource "aws_route53_record" "record" {
   zone_id = var.zone_id
   name    = "${var.name}-${var.env}"
@@ -17,7 +16,6 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip]
 
 }
-
 resource "null_resource" "ansible" {
 	depends_on = [aws_route53_record.record]
 	provisioner "remote-exec" {
--- variable.tf
+++ variable.tf
@@ -7,3 +7,5 @@ variable "env"{}
 variable "zone_id" {}
 
 variable "zone_name" {}
+
+variable "vpc_security_group_ids" {}
