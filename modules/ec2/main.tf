746b7e9a3d00a3d7863ccf9991503c7a04afd967
Gopi Chand <gopi110531@gmail.com>
2026-09-02 12:14:58 -0500

    Roboshop code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,22 +6,24 @@ resource "aws_instance" "instance" {
     Name = "${var.name}-${var.env}"
   }
 
-  provisioner "local-exec" {
-	  command = <<ANSIBLE
-cd /home/ec2-user/roboshop-ansible
-make role_name="$(var.name)"
-ANSIBLE
- }
-
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "record" {
   zone_id = var.zone_id
   name    = "${var.name}-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 
-
 }
+
+resource "null_resource" "ansible" {
+	depends_on = [aws_route53_record.record]
+	provisioner "local-exec" {
+	  command = <<ANSIBLE
+cd /home/ec2-user/roboshop-ansible
+make role_name="$(var.name)"
+ANSIBLE
+ }
+	}
 
