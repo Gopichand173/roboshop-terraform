326e66d927968111b3336d22b6fe31ff4f6b93b1
Gopi Chand <gopi110531@gmail.com>
2026-09-07 22:56:36 -0500

    Roboshop code

--- tools/provider.tf
+++ tools/provider.tf
@@ -1,11 +1,11 @@
 provider "aws" {
 	region = "us-east-1"
-	}
+}
 
 terraform {
 	backend "s3"{
       bucket = "terraform-g85"
       key    = "roboshop-terraform/environments/dev/state.tfvars"
       region = "us-east-1"
- }
+    }
 }
